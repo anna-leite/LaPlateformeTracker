289d0584c14515e0634cde851dd237075f060758
nyxx <leila.wilde@laplateforme.io>
2025-07-11 10:22:20 +0100

    refactor: init-schema and seed data

---
+++ src/main/java/com/example/util/DBConnectionManager.java
@@ -0,0 +1,41 @@
+package com.example.util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnectionManager {
+    private static final String PROPERTIES_FILE = "src/main/resources/application.properties";
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
+
+    static {
+        try (FileInputStream input = new FileInputStream(PROPERTIES_FILE)) {
+            Properties properties = new Properties();
+            properties.load(input);
+            URL = properties.getProperty("db.url");
+            USER = properties.getProperty("db.user");
+            PASSWORD = properties.getProperty("db.password");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
